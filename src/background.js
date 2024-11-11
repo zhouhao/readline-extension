56813b4c70317a172bb009f9a6d6afab5bed2388
SaltyEgg <hzhou.me@saltyee.com>
2024-11-10 22:25:09 -0800

    :sparkles: not perfect toggle

--- src/background.js
+++ src/background.js
@@ -1,13 +1,21 @@
 'use strict';
+let enabled = false; // shared across all tabs, not correct here.
 
 chrome.action.onClicked.addListener((tab) => {
-  if (tab.url.startsWith('http')) {
+  if (!tab.url.startsWith('http')) return;
+
+  enabled = !enabled;
+  if(enabled) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       files: ['contentScript.js']
     });
-
     chrome.scripting.insertCSS({
+      target: { tabId: tab.id },
+      files: ['contentScript.css']
+    });
+  } else {
+    chrome.scripting.removeCSS({
       target: { tabId: tab.id },
       files: ['contentScript.css']
     });
