c13d75d80b092546b4238996ad2c404489072874
SaltyEgg <hzhou.me@saltyee.com>
2024-11-10 22:05:43 -0800

    :sparkles: inject js and css dynamically

--- src/background.js
+++ src/background.js
@@ -1,6 +1,15 @@
 'use strict';
 
-chrome.action.onClicked.addListener(tab => {
+chrome.action.onClicked.addListener((tab) => {
+  if (tab.url.startsWith('http')) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['contentScript.js']
+    });
 
-
+    chrome.scripting.insertCSS({
+      target: { tabId: tab.id },
+      files: ['contentScript.css']
+    });
+  }
 });
