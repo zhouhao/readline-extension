57ce53c1c7a4c14794897b7c531b97cf33a59af5
SaltyEgg <hzhou.me@saltyee.com>
2024-11-10 21:45:28 -0800

    :bug: fix scroll event fix #1

--- src/contentScript.js
+++ src/contentScript.js
@@ -3,14 +3,31 @@ import './contentScript.css';
 
 const body = document.querySelector('body');
 const follower = document.createElement('div');
-follower.id = 'follower';
+follower.id = 'shark-eagle-readline-bar';
 body.appendChild(follower);
 
-document.addEventListener('mousemove', (event) => {
-  const x = event.pageX;
-  const y = event.pageY;
+let clientX = 0;
+let clientY = 0;
 
+const updateReadBar = (event) => {
   // Offset the position to center the div on the cursor
   follower.style.left = '0';
-  follower.style.top = y - follower.offsetHeight / 2 + 'px';
+  follower.style.top = event.pageY - follower.offsetHeight / 2 + 'px';
+};
+
+document.addEventListener('mousemove', (event) => {
+  clientX = event.clientX;
+  clientY = event.clientY;
+  updateReadBar(event);
+});
+
+document.addEventListener('scroll', () => {
+  const event = new MouseEvent('mousemove', {
+    view: window,
+    bubbles: true,
+    cancelable: true,
+    clientX: clientX,
+    clientY: clientY,
+  });
+  document.dispatchEvent(event);
 });
